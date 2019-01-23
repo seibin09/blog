a559e3341bcc7c8585ad082211646039271946ec
YoungBoss <YoungBoss@DESKTOP-JBOFO75>
2019-01-23 17:36:20 +0900

    static PropertySourcesPlaceholderConfigurer 앞에 static 을 붙여야 됨 이유는 모름
    Signed-off-by: YoungBoss <YoungBoss@DESKTOP-JBOFO75>

--- blog/src/main/java/co/kr/lim/config/ApplicationConfig.java
+++ blog/src/main/java/co/kr/lim/config/ApplicationConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -55,7 +56,7 @@ public class ApplicationConfig {
     }
     
     @Bean
-    public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
-    	return new PropertyPlaceholderConfigurer();
-    }
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 }

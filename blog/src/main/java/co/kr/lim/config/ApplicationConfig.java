9f381fbc7c919ff2ef7eeb6e8288786053a9a2a7
YoungBoss <YoungBoss@DESKTOP-JBOFO75>
2019-01-25 17:20:42 +0900

    트랜잭션 구현체 선언을 HibernateTransactionManager로 하니깐 안됨 JpaTransactionManager 하니깐 잘됨 머지?

--- blog/src/main/java/co/kr/lim/config/ApplicationConfig.java
+++ blog/src/main/java/co/kr/lim/config/ApplicationConfig.java
@@ -1,5 +1,8 @@
 package co.kr.lim.config;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.modelmapper.ModelMapper;
@@ -9,9 +12,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -26,24 +33,24 @@ import co.kr.lim.common.interceptor.CommonInterceptor;
 @Configuration
 @EnableWebMvc
 @WebAppConfiguration
-@ComponentScan(basePackages="co.kr.lim")
+@ComponentScan(basePackages = "co.kr.lim")
 @PropertySource("classpath:application.properties")
+@EnableJpaRepositories(basePackages = "co.kr.lim")
 @EnableTransactionManagement
-@EnableJpaRepositories
 public class ApplicationConfig extends WebMvcConfigurerAdapter {
-	
+
 	@Value("${jdbc.driverClassName}")
 	private String driverClassName;
-	
-    @Value("${jdbc.url}")
-    private String url;
-    
-    @Value("${jdbc.username}")
-    private String userName;
-    
-    @Value("${jdbc.password}")
-    private String password;
-    
+
+	@Value("${jdbc.url}")
+	private String url;
+
+	@Value("${jdbc.username}")
+	private String userName;
+
+	@Value("${jdbc.password}")
+	private String password;
+
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -53,29 +60,64 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter {
 		dataSource.setPassword(password);
 		return dataSource;
 	}
-	
-    @Bean
-    public PlatformTransactionManager txManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
-    
-    @Bean
-    public ModelMapper mapper() {
-    	return new ModelMapper();
-    }
-    
-    @Bean
-	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
-		return new PropertySourcesPlaceholderConfigurer();
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String[] { "co.kr.lim" });
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+		return em;
 	}
-    
-    @Override
-	public void addInterceptors(InterceptorRegistry registry) {
-    	registry.addInterceptor(new CommonInterceptor());
+
+	public Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		return properties;
+	}
+
+//	@Bean
+//	public PlatformTransactionManager transactionManager() {
+//		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//		transactionManager.setDataSource(dataSource());
+//		transactionManager.setSessionFactory((SessionFactory) entityManagerFactory().getObject());
+//		transactionManager.setHibernateManagedSession(false);
+//		return transactionManager;
+//	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
 	}
 
 	@Bean
-    public ObjectMapper objectMapper() {
-    	return new ObjectMapper();
-    }
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new CommonInterceptor());
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
 }

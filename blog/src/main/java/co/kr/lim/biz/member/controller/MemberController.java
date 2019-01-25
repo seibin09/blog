c5651df530551318b8f9839f0b80a0ee1887b39e
YoungBoss <YoungBoss@DESKTOP-JBOFO75>
2019-01-25 17:30:17 +0900

    @ExceptionHandler 추가
    Signed-off-by: YoungBoss <YoungBoss@DESKTOP-JBOFO75>

--- blog/src/main/java/co/kr/lim/biz/member/controller/MemberController.java
+++ blog/src/main/java/co/kr/lim/biz/member/controller/MemberController.java
@@ -3,23 +3,22 @@ package co.kr.lim.biz.member.controller;
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import co.kr.lim.MemberControllerTest;
 import co.kr.lim.biz.member.bean.Member;
 import co.kr.lim.biz.member.bean.MemberDto;
 import co.kr.lim.biz.member.service.MemberService;
 import co.kr.lim.common.ErrorResponse;
+import co.kr.lim.common.exception.UserDuplicatedException;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -46,4 +45,14 @@ public class MemberController {
 		memberService.memberCreate(create);
 		return new ResponseEntity<>(modelMapper.map(create, Member.class), HttpStatus.CREATED);
 	}
+	
+	@ExceptionHandler(UserDuplicatedException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorResponse handleUserDuplicatedException(UserDuplicatedException e) {
+		ErrorResponse errorResponse = new ErrorResponse();
+		errorResponse.setMessage("[" + e.getUsername() + "] 중복된 username 입니다.");
+		errorResponse.setCode("duplicated.username.exception");
+		return errorResponse;
+	}
+	
 }
